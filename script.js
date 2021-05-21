548efedb24e7c1952d846a8a99dbbd2c58af4796
Pavan Darji <84569753+PavanDarji@users.noreply.github.com>
2021-05-21 05:08:03 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,15 +5,13 @@ const geoDisplay = document.querySelector(".geo-display");
 const device = document.querySelector(".device");
 
 
-
 lookUpBtn.addEventListener("click", () => {
 
     axios.get("https://ipapi.co/json/").then(response => {
         ipDisplay.textContent = `Your IP Address  : ${response.data.ip}`;
         locationDisplay.textContent = `Your Current Location: ${response.data.city},${response.data.region},${response.data.country_name}`;
         geoDisplay.textContent = `Your GEO Address : ${response.data.latitude},${response.data.longitude}`;
-        device.textContent = `Your Network Device  : ${response.data.org}`; 
-       
+        device.textContent = `Your Network Device  : ${response.data.org}`;
         
     });
 });

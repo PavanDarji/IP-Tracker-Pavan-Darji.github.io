c4817d9ecfb82e390520a201f0ca63a4e0fb5bac
Pavan Darji <84569753+PavanDarji@users.noreply.github.com>
2021-05-21 04:45:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const lookUpBtn = document.querySelector(".lookup-btn");
 const ipDisplay = document.querySelector(".ip-display");
 const locationDisplay = document.querySelector(".location-display");
 const geoDisplay = document.querySelector(".geo-display");
-const device = document.querySelector(".device");
+
 
 
 lookUpBtn.addEventListener("click", () => {
@@ -11,7 +11,7 @@ lookUpBtn.addEventListener("click", () => {
         ipDisplay.textContent = `Your IP Address  : ${response.data.ip}`;
         locationDisplay.textContent = `Your Current Location: ${response.data.city},${response.data.region},${response.data.country_name}`;
         geoDisplay.textContent = `Your GEO Address : ${response.data.latitude},${response.data.longitude}`;
-        device.textContent = `Your Network Device  : ${response.data.org}`;
+       
         
     });
 });
